5330eed763b91130a000fa9aa09b4806ba850399
ShehaniWageesha <it19113746@my.sliit.lk>
2022-05-09 08:17:23 +0530

    Backend modified

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const userRoutes = require("./v1/user.route");
+const diaryRoutes = require("./v1/diary.route");
 
 // base route - /api
 const router = express.Router();
@@ -8,8 +9,8 @@ const router = express.Router();
 // Health check route
 router.get("/v1/health", (req, res) => res.status(200).send("UP"));
 
-// v1 user routes
+// v1 routes
 router.use("/v1/user", userRoutes);
-router.use("/v1/todo", userRoutes);
+router.use("/v1/diary", diaryRoutes);
 
 module.exports = router;
